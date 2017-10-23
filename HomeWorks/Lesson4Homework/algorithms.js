07b3c9ad06e67f52110ddf89cec64310528d5790
BatyaYaStarayus <iwankozlov1989@gmail.com>
2017-10-24 01:40:27 +0300

    updated homework for lesson 4

--- HomeWorks/Lesson4Homework/algorithms.js
+++ HomeWorks/Lesson4Homework/algorithms.js
@@ -41,29 +41,125 @@ getPrimeNumbersInRange(2, 10);
 
 // Task2
 
-//@todo
+var obj = {
+    className: 'open menu'
+}
 
+function addClass(obj, cls) {
+
+    var arrayOfClassNames = obj.className.split(" ");
+
+    if(arrayOfClassNames.indexOf(cls) === -1) {
+        arrayOfClassNames.push(cls);
+    }
+
+    obj.className = arrayOfClassNames.join(" ");
+
+    return obj;
+}
+
+addClass(obj, 'new'); // obj.className='open menu new'
+addClass(obj, 'open'); // без изменений (класс уже существует)
+addClass(obj, 'me'); // obj.className='open menu new me'
+
+console.log( obj.className ); // "open menu new me"
 
 // Task3
 
-//@todo
+var obj = {
+    className: 'open open menu'
+}
 
+function removeClass(obj, cls) {
+
+    var arrayOfClassNames = obj.className.split(" ");
+    var indexOfClsInArray = arrayOfClassNames.indexOf(cls);
+
+    if(indexOfClsInArray !== -1) {
+        arrayOfClassNames.splice(indexOfClsInArray, 1);
+        obj.className = arrayOfClassNames.join(" ");
+        removeClass(obj, cls);
+    }
+
+    return obj;
+}
+
+removeClass(obj, 'open'); // obj.className='menu'
+removeClass(obj, 'blabla'); // без изменений (нет такого класса)
 
 // Task4
 
-//@todo
+var sum = 0;
 
+function getSumOfNumbers() {
+
+    var number = +prompt("Enter any number");
+
+    if( (!isNaN(number)) && (number !== null) ) {
+        sum += number;
+        getSumOfNumbers();
+    }
+    return sum;
+}
+
+alert(getSumOfNumbers());
 
 // Task5
 
-//@todo
+function fib(n) {
+
+    var fibArray = [1, 1];
+
+    for (var i = 2; i < n; i++) {
+        fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    }
+    alert(fibArray[n-1]);
+}
+
+fib(77);
 
 // Task6
 
-//@todo
+function setStringLengthTwenty(string) {
+
+    var notLongerThanTwentySymbolsString = "";
+
+    if (string.length > 20) {
+        notLongerThanTwentySymbolsString += (string.substr(0, 20) + "...");
+    } else {
+        notLongerThanTwentySymbolsString = string;
+    }
+    return notLongerThanTwentySymbolsString;
+}
+
+setStringLengthTwenty("123456789012345678901234");
 
 // Task7
 
-//@todo
+var tasksCompleted = {
+    'Anna': 29,
+    'Serg': 35,
+    'Elena': 1,
+    'Anton': 99
+};
+
+function getMaxValueWorker(obj) {
+
+    var workersArray = [];
+    var valuesArray = [];
+    var i;
+
+    for (i in obj) {
+        workersArray.push(i);
+        valuesArray.push(obj[i]);
+    }
+
+    var maxValue = valuesArray.sort()[valuesArray.length - 1];
+    var workerWithMaxValue = workersArray[valuesArray.indexOf(maxValue)];
+
+    return workerWithMaxValue;
+}
+
+getMaxValueWorker(tasksCompleted);
 
 
