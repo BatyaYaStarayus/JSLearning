ceade2839bb28672958a799f7ca6beeeb1cb3293
BatyaYaStarayus <iwankozlov1989@gmail.com>
2017-10-30 00:27:59 +0200

    updated Lesson6Homework

--- HomeWorks/Lesson6Homework/dom.js
+++ HomeWorks/Lesson6Homework/dom.js
@@ -87,16 +87,105 @@ for (var i = 0; i < attributesArray.length; i++) {
 
 // Task4
 
+var paragraph = document.body.getElementsByTagName("p");
 
+paragraph[0].style.fontFamily = "Times New Roman";
+paragraph[0].style.fontSize = "50px";
+paragraph[0].style.color = "red";
 
 // Task5
+
+var element;
+
+if(element.childNodes < 1) {
+    return false
+}
+
 // Task6
+
+
+
 // Task7
 // Task8
 // Task9
+
+
+
 // Task10
+
+//is there need to create elements with class "www", if function below works for
+// https://github.com/kl2karpenko/JS-practise-NetEnt/blob/master/lesson6/dom.md? :)
+function addTagNameToClass(elementClass) {
+
+    var elementsArray = document.querySelectorAll("." + elementClass);
+
+    for (var i = 0; i < elementsArray.length; i++) {
+
+        var newClassWithTagName = elementClass + elementsArray[i].tagName.toLowerCase();
+        elementsArray[i].setAttribute("class", newClassWithTagName);
+    }
+}
+
+addTagNameToClass("facebox");
+
 // Task11
+
+//task is absent
+
 // Task12
+
+function createList(elementsArray, ordered) {
+
+    if(!ordered) {
+        var ul = document.createElement("ul");
+        document.body.appendChild(ul);
+
+        for (var i = 0; i < elementsArray.length; i++) {
+            var li = document.createElement("li");
+            ul.appendChild(li);
+            li.innerText = elementsArray[i];
+        }
+    } else {
+        var ol = document.createElement("ol");
+        document.body.appendChild(ol);
+
+        for (var j = 0; j < elementsArray.length; j++) {
+            var li = document.createElement("li");
+            ol.appendChild(li);
+            li.innerText = elementsArray[j];
+        }
+    }
+}
+
+createList(["1", "2", "3"], true);
+
 // Task13
+
+//using updated function from Task12 :)
+createList(["1", "2", "3"], false);
+
 // Task14
+//todo
+function setCharsRandomColor(elementQuery) {
+
+    var element = document.querySelector(elementQuery);
+    var elementText = element.innerText.split("");
+
+    for (var i = 0; i < elementText.length; i++) {
+
+        var randomColor = getRandomColorInRGB();
+
+        if (randomColor !== (elementText[i] - 1).style.color) {
+            elementText[i].style.color = randomColor;
+        }
+    }
+    var coloredString = elementText.join("");
+
+    element.innerText = coloredString;
+}
+
+setCharsRandomColor("p");
+
 // Task15
+
+//duplicates Task14
