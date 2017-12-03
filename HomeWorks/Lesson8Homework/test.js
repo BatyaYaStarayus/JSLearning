06344fc075d6ee1225a14a935a67a6df8d9f6fe5
BatyaYaStarayus <iwankozlov1989@gmail.com>
2017-12-03 18:28:19 +0200

    updated Lesson8Homework

--- HomeWorks/Lesson8Homework/test.js
+++ HomeWorks/Lesson8Homework/test.js
@@ -1,28 +1,14 @@
-function showTimeToMidnight() {
-    var div = document.querySelector("div");
-    var dateObject = new Date();
+var image = document.querySelector("img");
+var framesCounter = 1;
 
-    var currentHours = dateObject.getHours();
-    var currentMinutes = dateObject.getMinutes();
-    var currentSeconds = dateObject.getSeconds();
+function startAnimation() {
+    image.src = "Frames/Frame" + framesCounter + ".png";
 
-    var hoursToMidnight = 23 - (+currentHours);
-    var minutesToMidnight = 59 - (+currentMinutes);
-    var secondsToMidnight = 59 - (+currentSeconds);
-
-    var timeToMidnightArray = [
-        hoursToMidnight.toString(),
-        minutesToMidnight.toString(),
-        secondsToMidnight.toString()
-    ];
-
-    for (var i = 0; i < timeToMidnightArray.length; i++) {
-        if (timeToMidnightArray[i].length === 1) {
-            timeToMidnightArray[i] = "0" + timeToMidnightArray[i];
-        }
+    if(framesCounter === 6) {
+        framesCounter = 1;
+    } else {
+        framesCounter++;
     }
-
-    div.innerHTML = timeToMidnightArray.join(":");
 }
 
-setInterval(showTimeToMidnight, 10);
+setInterval(startAnimation, 200);
--- HomeWorks/Lesson8Homework/timers-intervals.js
+++ HomeWorks/Lesson8Homework/timers-intervals.js
@@ -24,10 +24,113 @@ function showAndIncrementNumber() {
 
 setInterval(showAndIncrementNumber, 500);
 
-
 // Task3
 
-//todo
+//index.html file
+// <!DOCTYPE html>
+// <html lang="en">
+//     <head>
+//     <meta charset="UTF-8">
+//     <title>Title</title>
+//     </head>
+//     <body>
+//     <div id="timeScreen"></div>
+//     <div id="buttonsBlock">
+//     <button id="startButton">Start</button>
+//     <button id="stopButton">Stop</button>
+//     <button id="pauseContinueButton">Pause</button>
+//     </div>
+//     <script src="test.js"></script>
+//     </body>
+//     </html>
+
+var startButton = document.querySelector("#startButton");
+var stopButton = document.querySelector("#stopButton");
+var pauseContinueButton = document.querySelector("#pauseContinueButton");
+var timeScreen = document.querySelector("#timeScreen");
+
+var currentTime = [0, 0, 0, 0];
+var startTime = [0, 0, 0, 0];
+
+var startCount;
+
+timeScreen.innerHTML = showTimeTwoNumbers(currentTime);
+
+startButton.addEventListener("click", startTimer);
+
+function startTimer() {
+    startCount = setInterval(increaseTime, 10);
+
+    startButton.removeEventListener("click", startTimer);
+
+    stopButton.addEventListener("click", stopTimer);
+    pauseContinueButton.addEventListener("click", pauseTimer);
+}
+
+function stopTimer() {
+    clearInterval(startCount);
+
+    startButton.addEventListener("click", startTimer);
+
+    pauseContinueButton.removeEventListener("click", pauseTimer);
+    pauseContinueButton.removeEventListener("click", continueTimer);
+
+    pauseContinueButton.innerHTML = "Pause";
+    timeScreen.innerHTML = showTimeTwoNumbers(startTime);
+
+    currentTime = [0, 0, 0, 0];
+}
+
+function pauseTimer() {
+    clearInterval(startCount);
+
+    timeScreen.innerHTML = showTimeTwoNumbers(currentTime);
+    pauseContinueButton.innerHTML = "Continue";
+
+    pauseContinueButton.addEventListener("click", continueTimer);
+
+    pauseContinueButton.removeEventListener("click", pauseTimer);
+}
+
+function continueTimer() {
+    startCount = setInterval(increaseTime, 10);
+
+    pauseContinueButton.innerHTML = "Pause";
+
+    pauseContinueButton.addEventListener("click", pauseTimer);
+
+    pauseContinueButton.removeEventListener("click", continueTimer);
+}
+
+function increaseTime() {
+    currentTime[3]++;
+
+    if(currentTime[3] === 100) {
+        currentTime[3] = 0;
+        currentTime[2]++;
+
+        if(currentTime[2] === 60) {
+            currentTime[2] = 0;
+            currentTime[1]++;
+
+            if(currentTime[1] === 60) {
+                currentTime[1] = 0;
+                currentTime[0]++;
+            }
+        }
+    }
+    timeScreen.innerHTML = showTimeTwoNumbers(currentTime);
+}
+
+function showTimeTwoNumbers(time) {
+    for(var i = 0; i < time.length; i++) {
+        if(time[i].toString().length === 1) {
+            time[i] = 0 + time[i].toString();
+        }
+    }
+
+    return time.join(":");
+}
 
 // Task4
 
@@ -49,7 +152,111 @@ setInterval(showCurrentTime, 10);
 
 // Task5
 
-//todo
+//index.html file
+// <!DOCTYPE html>
+// <html lang="en">
+//     <head>
+//     <meta charset="UTF-8">
+//     <title>Title</title>
+//     </head>
+//     <body>
+//     <div id="timeScreen"></div>
+//     <div id="buttonsBlock">
+//     <button id="startButton">Start</button>
+//     <button id="stopButton">Stop</button>
+//     <button id="pauseContinueButton">Pause</button>
+//     </div>
+//     <script src="test.js"></script>
+//     </body>
+//     </html>
+
+var startButton = document.querySelector("#startButton");
+var stopButton = document.querySelector("#stopButton");
+var pauseContinueButton = document.querySelector("#pauseContinueButton");
+var timeScreen = document.querySelector("#timeScreen");
+
+var currentTime = [0, 1, 0, 0];
+var startTime = [0, 1, 0, 0];
+
+var startCount;
+
+timeScreen.innerHTML = showTimeTwoNumbers(currentTime);
+
+startButton.addEventListener("click", startTimer);
+
+function startTimer() {
+    startCount = setInterval(increaseTime, 10);
+
+    startButton.removeEventListener("click", startTimer);
+
+    stopButton.addEventListener("click", stopTimer);
+    pauseContinueButton.addEventListener("click", pauseTimer);
+}
+
+function stopTimer() {
+    clearInterval(startCount);
+
+    startButton.addEventListener("click", startTimer);
+
+    pauseContinueButton.removeEventListener("click", pauseTimer);
+    pauseContinueButton.removeEventListener("click", continueTimer);
+
+    pauseContinueButton.innerHTML = "Pause";
+    timeScreen.innerHTML = showTimeTwoNumbers(startTime);
+
+    currentTime = [0, 1, 0, 0];
+}
+
+function pauseTimer() {
+    clearInterval(startCount);
+
+    timeScreen.innerHTML = showTimeTwoNumbers(currentTime);
+    pauseContinueButton.innerHTML = "Continue";
+
+    pauseContinueButton.addEventListener("click", continueTimer);
+
+    pauseContinueButton.removeEventListener("click", pauseTimer);
+}
+
+function continueTimer() {
+    startCount = setInterval(increaseTime, 10);
+
+    pauseContinueButton.innerHTML = "Pause";
+
+    pauseContinueButton.addEventListener("click", pauseTimer);
+
+    pauseContinueButton.removeEventListener("click", continueTimer);
+}
+
+function increaseTime() {
+    currentTime[3]--;
+
+    if(currentTime[3] < 0) {
+        currentTime[3] = 99;
+        currentTime[2]--;
+
+        if(currentTime[2] < 0) {
+            currentTime[2] = 59;
+            currentTime[1]--;
+
+            if(currentTime[1] < 0) {
+                currentTime[1] = 59;
+                currentTime[0]--;
+            }
+        }
+    }
+    timeScreen.innerHTML = showTimeTwoNumbers(currentTime);
+}
+
+function showTimeTwoNumbers(time) {
+    for(var i = 0; i < time.length; i++) {
+        if(time[i].toString().length === 1) {
+            time[i] = 0 + time[i].toString();
+        }
+    }
+
+    return time.join(":");
+}
 
 // Task6
 
@@ -84,7 +291,33 @@ setInterval(showTimeToMidnight, 10);
 
 // Task7
 
-//todo
+//index.html file
+// <!DOCTYPE html>
+// <html lang="en">
+//     <head>
+//     <meta charset="UTF-8">
+//     <title>Title</title>
+//     </head>
+//     <body>
+//     <img>
+//     <script src="test.js"></script>
+//     </body>
+//     </html>
+
+var image = document.querySelector("img");
+var framesCounter = 1;
+
+function startAnimation() {
+    image.src = "Frames/Frame" + framesCounter + ".png";
+
+    if(framesCounter === 6) {
+        framesCounter = 1;
+    } else {
+        framesCounter++;
+    }
+}
+
+setInterval(startAnimation, 200);
 
 // Task8
 
